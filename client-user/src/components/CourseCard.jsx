0cd54405f9dc6f6a2bef55c15cc7e2e55abb0fd7
Jeysiva-apjs <jeysiva.apjs@gmail.com>
2023-09-04 14:45:32 +0530

    updated

--- client-user/src/components/CourseCard.jsx
+++ client-user/src/components/CourseCard.jsx
@@ -32,14 +32,45 @@ function CourseCard(props) {
             image={props.course.imageLink}
             title={props.course.title}
           />
-          <CardContent>
+          <CardContent
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-between",
+            }}
+          >
             <Typography
               gutterBottom
               variant="h5"
               component="div"
-              style={{ fontWeight: "700", color: isMoveOver && "#bc1c44" }}
+              style={{
+                fontWeight: "700",
+                color: isMoveOver && "#bc1c44",
+                overflow: "hidden",
+                textOverflow: "ellipsis",
+                display: "-webkit-box",
+                "-webkit-line-clamp": 2, // Set the maximum number of lines to 2
+                "-webkit-box-orient": "vertical",
+              }}
             >
               {props.course.title}
+            </Typography>
+            <br />
+            <Typography
+              gutterBottom
+              variant="h8"
+              component="div"
+              style={{
+                fontWeight: "50",
+                fontFamily: "inherit",
+                display: "-webkit-box",
+                WebkitLineClamp: 1,
+                WebkitBoxOrient: "vertical",
+                overflow: "hidden",
+                textOverflow: "ellipsis",
+              }}
+            >
+              {props.course.description}
             </Typography>
             <br />
             <Typography
